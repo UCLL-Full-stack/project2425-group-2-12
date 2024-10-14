a1921caf5e0a70392403dff166a1876cd3b9da68
Senne <senne.rosseel@student.ucll.be>
2024-10-14 14:41:44 +0200

    Modifying user.ts

--- back-end/model/user.ts
+++ back-end/model/user.ts
@@ -1,14 +1,18 @@
+import { Profile } from './profile';
+
 export class User {
     // private variable instances
     private id?: number; // id is optional (?)
     private username: string;
     private password: string;
+    private profile: Profile;
 
     // constructor
-    constructor(user: { id?: number; username: string; password: string }) {
+    constructor(user: { id?: number; username: string; password: string; profile: Profile }) {
         this.id = user.id;
         this.username = user.username;
         this.password = user.password;
+        this.profile = user.profile;
     }
 
     // getters
@@ -22,5 +26,9 @@ export class User {
 
     getFirstName(): string {
         return this.password;
+    }
+
+    getProfile(): Profile {
+        return this.profile;
     }
 }
