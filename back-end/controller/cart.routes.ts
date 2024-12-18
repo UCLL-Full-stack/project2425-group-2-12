e6b2107561953fb0be49f5d4818e206a1b8fd77c
robbE <robbenevens12@gmail.com>
2024-12-18 20:52:25 +0100

    testable in swagger

--- back-end/controller/cart.routes.ts
+++ back-end/controller/cart.routes.ts
@@ -3,6 +3,28 @@ import cartService from '../service/cart.service';
 
 const cartRouter = express.Router();
 
+/**
+ * @swagger
+ * /cart/{username}:
+ *   get:
+ *     summary: Get cart items by username
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Username of the user
+ *     responses:
+ *       200:
+ *         description: A list of cart items
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ */
 cartRouter.get('/:username', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { username } = req.params;
@@ -13,6 +35,37 @@ cartRouter.get('/:username', async (req: Request, res: Response, next: NextFunct
     }
 });
 
+/**
+ * @swagger
+ * /cart/{username}:
+ *   post:
+ *     summary: Add a product to the cart
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Username of the user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               productId:
+ *                 type: string
+ *               quantity:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Updated cart
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ */
 cartRouter.post('/:username', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { username } = req.params;
@@ -24,6 +77,32 @@ cartRouter.post('/:username', async (req: Request, res: Response, next: NextFunc
     }
 });
 
+/**
+ * @swagger
+ * /cart/{username}/{productId}:
+ *   delete:
+ *     summary: Remove a product from the cart
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Username of the user
+ *       - in: path
+ *         name: productId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the product to remove
+ *     responses:
+ *       200:
+ *         description: Updated cart
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ */
 cartRouter.delete(
     '/:username/:productId',
     async (req: Request, res: Response, next: NextFunction) => {
